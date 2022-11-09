af3f90aa0d83b63a8821af3c332bd2a010b56304
Kara Bassman <110419577+Kara4Sea@users.noreply.github.com>
2022-11-09 11:34:35 -0800

    adding bonus requirements

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -16,6 +16,7 @@
 
 SELECT (backers_count), cf_id
 FROM campaign
+WHERE outcome = 'live'
 ORDER BY backers_count DESC;
 
 
@@ -37,26 +38,39 @@ ORDER BY count DESC;
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
 
-SELECT cf_id, goal, pledged, (goal - pledged)
+SELECT first_name, last_name, email, (goal - pledged) AS "Remaining Goal Amount"
 INTO email_contacts_remaining_goal_amount
-FROM campaign
--- WHERE (goal - pledged) = "Remaining_Goal_Amount"
-WHERE (goal - pledged) >0
+FROM contacts as ct
+JOIN campaign as c
+ON ct.contact_id = c.contact_id
+WHERE outcome = 'live'
 ORDER BY (goal - pledged) DESC;
 
-SELECT * FROM email_contacts_remaining_goal_amount;
 
-ALTER TABLE email_contacts_remaining_goal_amount
-RENAME COLUMN column TO Remaining Goal Amount;
+
+
+
+
+-- SELECT cf_id, goal, pledged, (goal - pledged)
+-- -- INTO email_contacts_remaining_goal_amount
+-- FROM campaign
+-- -- WHERE (goal - pledged) = "Remaining_Goal_Amount"
+-- WHERE (goal - pledged) >0
+-- ORDER BY (goal - pledged) DESC;
+
+-- SELECT * FROM email_contacts_remaining_goal_amount;
+
+-- ALTER TABLE email_contacts_remaining_goal_amount
+-- RENAME COLUMN column TO Remaining Goal Amount;
 
 -- Joining departments and dept_manager tables
-SELECT backers.first_name,
-     backers.last_name,
-     backers.email,
-     dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+-- SELECT backers.first_name,
+--      backers.last_name,
+--      backers.email,
+--      dept_manager.to_date
+-- FROM departments
+-- INNER JOIN dept_manager
+-- ON departments.dept_no = dept_manager.dept_no;
 
 
 -- Check the table
@@ -67,7 +81,14 @@ ON departments.dept_no = dept_manager.dept_no;
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
 
-
+SELECT email,first_name, last_name, c.cf_id, 
+company_name, description, end_date, (goal - pledged) AS "Left of Goal"
+INTO email_backers_remaining_goal_amount
+FROM backers as b
+JOIN campaign as c
+ON b.cf_id = c.cf_id
+WHERE outcome = 'live'
+ORDER BY (last_name, email);
 
 -- Check the table
 
